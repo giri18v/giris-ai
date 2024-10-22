fd82ced5bd6afaeac93b0e27b7345ca06eff0409
giri18v <giri18v@outlook.com>
2024-10-22 18:55:46 +0100

    Import issues resoloved

--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  webpack: (config) => {
+    config.externals = [...config.externals, 'canvas', 'jsdom'];
+    return config;
+  },
 }
 
 module.exports = nextConfig
